3cc3620699d0341ac7f132230c2f5bccc5d3e13d
Siddarth Naik <siddarthnaik2131@gmail.com>
2024-08-02 13:02:08 +0530

    Add functions to check for new files and get files

--- helper_functions.py
+++ helper_functions.py
@@ -11,3 +11,16 @@ def check_for_devices(userid:int) -> bool:
         return False , result
     else:
         return True ,result
+
+def get_files(userid, device_name):
+    result = sp.run(["ls",f"/run/user/{userid}/gvfs/{device_name}/"], capture_output=True).stdout.decode().replace("\n"," ")
+    return list(result.split(" "))
+
+def check_for_new_file(userid, device_name, inital_files):
+    current_files = get_files(userid,device_name)
+    if current_files == inital_files:
+        return False,[]
+    else:
+        new_files = set(current_files) - set(inital_files)
+        return True,list(new_files)
+    
